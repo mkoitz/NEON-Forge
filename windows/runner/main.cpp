8677e962e5eb1f084741b76c3d44f4b25ae3c245
Michael Koitz <michael.koitz@gmail.com>
2026-04-18 20:21:30 +0200

    Update app branding and launcher icons

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"neon_forge", origin, size)) {
+  if (!window.Create(L"NEON Forge", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
